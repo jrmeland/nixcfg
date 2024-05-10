3d794c65701e4fb944f54922fcd0bb8a5a2c2286
Josh Melander <joshmelander@gmail.com>
2024-05-09 20:52:52 -0700

    Install manix

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -24,6 +24,7 @@
     pkgs.nixpkgs-fmt
     pkgs.nil
     pkgs.llm
+    pkgs.manix
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
