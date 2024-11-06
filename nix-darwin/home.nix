3e86c876ef0722ee490a39ec0d07f218ae7dbff4
Josh Melander <joshmelander@gmail.com>
2024-11-05 20:17:37 -0800

    neat comand for github registry login

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -176,6 +176,8 @@ in
       export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+      alias login_github_registry="gh auth token | docker login ghcr.io -u jrmeland --password-stdin"
     '';
   };
 
